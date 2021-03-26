8057086700f4fb22912f850258dca9e2345809f1
Gianinna Marquez <73908034+Ninna-log@users.noreply.github.com>
2021-03-26 18:39:41 -0300

    Adding a new client

--- src/main/java/com/pickupapp/pickupapp/PickupappApplication.java
+++ src/main/java/com/pickupapp/pickupapp/PickupappApplication.java
@@ -31,21 +31,25 @@ public class PickupappApplication {
 			Customer customer2 = new Customer("pepito@gmail.com", "Pepito", "Pérez",passwordEncoder.encode("24"));
 			Customer customer3 = new Customer("carlitos@gmail.com", "Carlos", "Díaz",passwordEncoder.encode("24"));
 			Customer customer4 = new Customer("maria@gmail.com", "María", "Díaz",passwordEncoder.encode("24"));
+			Customer customer5 = new Customer("nicolas@gmail.com", "Nicolás", "Marquart",passwordEncoder.encode("1234"));
 
 			customerRepository.save(customer2);
 			customerRepository.save(customer1);
 			customerRepository.save(customer3);
 			customerRepository.save(customer4);
+			customerRepository.save(customer5);
 
 			Order order1 = new Order(LocalDate.now(), LocalTime.now().plusHours(1), customer1, 5, 1500.50);
 			Order order2 = new Order(LocalDate.now(), LocalTime.now().plusHours(2), customer2, 15, 2998.60);
 			Order order3 = new Order(LocalDate.now(), LocalTime.now().plusHours(3), customer3, 18, 4590.40);
 			Order order4 = new Order(LocalDate.now(), LocalTime.now().plusHours(4), customer4, 9, 1870.30);
+			Order order5 = new Order(LocalDate.now(), LocalTime.now().plusHours(4), customer4, 7, 1080.30);
 
 			orderRepository.save(order1);
 			orderRepository.save(order2);
 			orderRepository.save(order3);
 			orderRepository.save(order4);
+			orderRepository.save(order5);
 
 			Product product1 = new Product("Chocolate", 300.00, customer1);
 			Product product2 = new Product("Helado", 400.00, customer2);
@@ -61,11 +65,13 @@ public class PickupappApplication {
 			CustomerProduct customerProduct2 = new CustomerProduct(customer2, product2, order2);
 			CustomerProduct customerProduct3 = new CustomerProduct(customer2, product3, order3);
 			CustomerProduct customerProduct4 = new CustomerProduct(customer3, product4, order4);
+			CustomerProduct customerProduct5 = new CustomerProduct(customer5, product4, order5);
 
 			customerProductRepository.save(customerProduct1);
 			customerProductRepository.save(customerProduct2);
 			customerProductRepository.save(customerProduct3);
 			customerProductRepository.save(customerProduct4);
+			customerProductRepository.save(customerProduct5);
 
 		};
 	}

40294a8b1dbea6de77a50211687b62b6b02bc668
Gianinna Marquez <73908034+Ninna-log@users.noreply.github.com>
2021-03-26 18:01:09 -0300

    Fixing error

--- src/main/resources/static/web/script/orders.js
+++ src/main/resources/static/web/script/orders.js
@@ -1,7 +1,7 @@
 function logout() {
     $.post("/api/logout")
         .done(function () {
-            location.replace("pickup.html")
+            location.replace("/web/pickup.html")
         })
         .fail(function () {
             alert("Ha ocurrido un error, por favor pruebe otra vez")
